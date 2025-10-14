d8ae22e8b76e6334a13e241ad692050484717f02
ImWagy <90405968+ImWagy@users.noreply.github.com>
2025-10-14 19:20:32 +0200

    Update backend.bash

--- backend.bash
+++ backend.bash
@@ -1,4 +1,8 @@
 npm init -y
-npm install express socket.io cors'
+npm install express socket.io cors
+npm install mysql2
+npm install express socket.io bcrypt jsonwebtoken cors
+npm install dotenv
 npm install express socket.io mysql2 bcrypt jsonwebtoken cors
 node server.js
+
